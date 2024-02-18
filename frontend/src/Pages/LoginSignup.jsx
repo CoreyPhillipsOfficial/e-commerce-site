862953b2051765d4466694aa535ed8327137b501
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-02-18 16:52:14 -0500

    signup page

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 export const LoginSignup = () => {
     return (
-        <div className='LoginSignup'>
+        <div className='loginsignup'>
             <div className="loginsignup-container">
                 <h1>Sign Up</h1>
                 <div className="loginsignup-fields">
@@ -14,6 +15,7 @@ export const LoginSignup = () => {
                 <p className="loginsignup-login">Already have an account? <span>Login Here</span> </p>
                 <div className="loginsignup-agree">
                     <input type="checkbox" name='' id='' />
+                    <p>By continuing, I  agree to the terms of use and privacy policy.</p>
                 </div>
             </div>
         </div>
