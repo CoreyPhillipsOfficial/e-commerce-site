2eb5b06c7002436180d946847946f7d6238d1b32
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-02-18 18:04:37 -0500

    created breadcrumbs.jsx

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import ShopContext from '../Context/ShopContext'
 
 export const Product = () => {
+    const { all_product } = useContext(ShopContext);
+    const { productId } = useParams();
+    const product = all_product.find((e) => e.id === Number(productId))
     return (
-        <div>Product</div>
+        <div>
+
+        </div>
     )
 }
+
+export default Product
