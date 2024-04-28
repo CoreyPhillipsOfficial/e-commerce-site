8590c5c67f125b55ec9ae072ef08264b6aaf7036
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-04-28 12:52:30 -0400

    addproduct endpoint working

--- backend/index.js
+++ backend/index.js
@@ -73,6 +73,24 @@ const Product = mongoose.model('Product', {
         type: Boolean,
         default: true,
     },
+});
+
+app.post('/addproduct', async (req, res) => {
+    const product = new Product({
+        id: req.body.id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log('Saved');
+    res.json({
+        success: true,
+        name: req.body.name,
+    })
 })
 
 app.listen(port, (error) => {
