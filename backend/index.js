9b5c03724a79b7659efc62ca8c09933e2461d358
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-04-29 18:01:31 -0400

    login endpoint successful

--- backend/index.js
+++ backend/index.js
@@ -170,10 +170,33 @@ app.post('/signup', async (req, res) => {
     res.json({ success: true, token })
 });
 
+// Creating endpoint for user login
+app.post('/login', async (req, res) => {
+    let user = await Users.findOne({ email: req.body.email });
+    if (user) {
+        const passCompare = req.body.password === user.password;
+        if (passCompare) {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const token = jwt.sign(data, 'secret_ecom');
+            res.json({ success: true, token });
+        }
+        else {
+            res.json({ success: false, errors: 'Wrong password' });
+        }
+    }
+    else {
+        res.json({ success: false, errors: 'Wrong email' })
+    }
+});
+
 app.listen(port, (error) => {
     if (!error) {
         console.log('Server running on port ' + port);
     } else {
         console.log('Error : ' + error);
-    }
+    };
 });
