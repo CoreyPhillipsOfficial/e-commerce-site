0bb637151b2401e29748b82a7dff3522ab7d0faf
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-04-30 17:23:54 -0400

    remove from cart successful

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -25,7 +25,7 @@ const ListProduct = () => {
             },
             body: JSON.stringify({ id: id })
         });
-        await fetchInfo();
+        // await fetchInfo();
     };
 
     return (
